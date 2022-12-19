018d6520b04cd2b6ed4ffeb7e2aa3d2235d6a606
Urusov Artur <urusovartur110@gmail.com>
2022-12-19 07:41:57 +0300

    Initial commit, переделал проект

---
+++ src/main/java/ru/kata/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
@@ -0,0 +1,16 @@
+package ru.kata.spring.boot_security.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+public class SpringBootSecurityDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityDemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ru/kata/spring/boot_security/demo/dao/RoleDao.java
@@ -0,0 +1,14 @@
+package ru.kata.spring.boot_security.demo.dao;
+
+import ru.kata.spring.boot_security.demo.model.Role;
+
+import java.util.List;
+import java.util.Set;
+
+public interface RoleDao {
+    List<Role> findAll();
+    void save(Role role);
+    Role findById(Long id);
+    void deleteById(Long id);
+    Role getRoleByName(String name);
+}
